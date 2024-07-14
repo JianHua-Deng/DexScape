c352f00280342c1ef76626956c75048bcd9f7d39
JianHua-Deng <metails1203@gmail.com>
2024-07-14 09:15:56 -0400

    fix api call of using axios for querying

--- src/Utils/APICalls/MangaDexApi.js
+++ src/Utils/APICalls/MangaDexApi.js
@@ -3,27 +3,40 @@ import axios from "axios";
 const title = 'Kanojyo to Himitsu to Koimoyou';
 const proxyUrl = "https://corsproxy.io/";
 const infoUrl = "https://api.mangadex.org/manga";
-const coverUrl = "https://uploads.mangadex.org/covers";
 
-const searchMangaUrl = `${proxyUrl}?${infoUrl}?title=${title}`;
-//const coverUrl = `${proxyUrl}${coverUrl}?`
 const id = "e0a8eefb-4859-44c1-aa44-6550618eae85";
 
-console.log(searchMangaUrl);
 
 async function searchMangas(){
     const resp = await axios({
         method: "GET",
-        url: `${searchMangaUrl}/manga`,
+        url: infoUrl,
+        proxy:{
+            url: proxyUrl,
+        },
         params: {
-            title: title,
+            title: title
         }
-    });
+    }).catch( e => {
+        console.log(e);
+    })
 
-    return resp.data.data;
+    return resp;
 
     
 }
+
+
+/*  
+async function searchMangas(){
+    return await axios.get(proxyUrl, {
+        params: {
+            url: infoUrl,
+            title: title,
+        }
+    }).catch(e => {console.log(e)});
+}
+*/
 
 export {searchMangas};
 
