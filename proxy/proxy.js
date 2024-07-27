bb0a8e21ad6ceafd64665e3d1f61fc19aaa93b8b
JianHua-Deng <metails1203@gmail.com>
2024-07-27 13:02:32 -0400

    edited headers to see if it can be fixed

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -35,7 +35,9 @@ const mangaCoversProxy = createProxyMiddleware({
         console.log('Proxying request:' + req.url);
     },
     onProxyRes: (proxyRes, req, res) => {
-        //proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+        proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+        proxyRes.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
         console.log('Received response for:' + req.url);
     },
 
