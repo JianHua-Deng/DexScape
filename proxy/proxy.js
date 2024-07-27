cb99b58704905efbe7c50e0acc14686bfd82feb3
JianHua-Deng <metails1203@gmail.com>
2024-07-27 12:27:49 -0400

    changed access control origin to see if it works

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -7,11 +7,35 @@ dotenv.config({ path: '../.env' });
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+/*
 app.use(cors({
     origin: 'https://manga-site-production.up.railway.app', //Railway app URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+*/
+
+// Set middleware of CORS 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://manga-site-production.up.railway.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -62,10 +86,6 @@ const mangaSearchProxy = createProxyMiddleware({
 app.use('/manga', mangaSearchProxy);
 app.use('/covers', mangaCoversProxy);
 
-// Add a catch-all route to serve your React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-  });
 
 app.listen(PORT, () => {
     console.log("It is currently running on PORT: " + PORT);
