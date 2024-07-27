76b523cd558cc5d3ca1cbd398d1a6960b8dfa6d4
JianHua-Deng <metails1203@gmail.com>
2024-07-27 12:56:34 -0400

    adding http to url to see if it fixes it

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -7,35 +7,11 @@ dotenv.config({ path: '../.env' });
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-/*
 app.use(cors({
     origin: 'https://manga-site-production.up.railway.app', //Railway app URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-*/
-
-// Set middleware of CORS 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://manga-site-production.up.railway.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'dist')));
--- src/Utils/APICalls/MangaDexApi.js
+++ src/Utils/APICalls/MangaDexApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const proxyUrl = process.env.RAILWAY_PUBLIC_DOMAIN;
+const proxyUrl = `https://${process.env.RAILWAY_PUBLIC_DOMAIN}` || 'http://localhost:5000';
 const queryMangasUrl = "https://api.mangadex.org/manga";
 const queryChaptersUrl = "https://api.mangadex.org/at-home/server/";
 
